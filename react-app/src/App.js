bfb83272c61db0b59ca631f54035d353b83fa7ed
Shandira <shandira.ferguson@gmail.com>
2023-02-04 21:42:03 -0800

    Part 2 complete w/ minor syntax revision needed - TBC

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Home from './Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import InventoryList from './InventoryList';
+import InventoryEdit from './InventoryEdit';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' exact element={<Home />} />
+          <Route path='/inventories' exact element={<InventoryList />} />
+          <Route path='/inventories/:id' element={<InventoryEdit />} />
+        </Routes>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
